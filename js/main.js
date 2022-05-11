8b2992a42dcce6601f664af9404d800d6575b14a
Ingmar Manuel Soares Medina <ingmar.soares.medina@gmail.com>
2022-05-11 21:16:40 +0100

    Added all the files

---
+++ js/main.js
@@ -0,0 +1,46 @@
+const hora = {
+    h : 00,
+    m: 00,
+    s: 00,
+    ms: 000,
+    addMs () {
+        hora.ms += 1
+        if (hora.ms == 1000) {
+            hora.ms = 000;
+            hora.s += 1;
+        }if (hora.s == 60) {
+            hora.s = 00;
+            hora.m += 1;
+        }if (hora.m == 60) {
+            hora.m = 00;
+            hora.h += 1;
+        }
+    }
+}
+const stopWatch = {
+    started : false,
+    duration () {
+        console.log(`${hora.h} Horas: ${hora.m} Minutos: ${hora.s} Segundos: ${hora.ms} Milisegundos`)
+    },
+    reset () {
+        hora.ms = 000;
+        hora.s = 00;
+        hora.m = 00;
+        hora.h = 00;
+        console.log('Cronometro a cero!')
+    },
+    start () {
+        if (stopWatch.started == false) {
+        intervalID = setInterval(hora.addMs, 1)
+        stopWatch.started = true 
+        console.log('Cronometro iniciado!')}
+        else {throw new Error ('Cronometro ya iniciado!!!!!')}
+    },
+    stop () {
+        if (stopWatch.started == true) {
+        clearInterval(intervalID)
+        stopWatch.started = false 
+        console.log('Cronometro detenido!')}
+        else {throw new Error ('Cronometro ya detenido!!!!!')}
+    }
+}
